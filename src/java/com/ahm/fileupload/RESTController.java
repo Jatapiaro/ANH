d5b17c1aef72a56ed2072d9599c319168f37021d
Jacobo <jacob.et.cetera@gmail.com>
2017-04-19 02:34:40 -0500

    REST file Upload

--- src/java/com/ahm/fileupload/RESTController.java
+++ src/java/com/ahm/fileupload/RESTController.java
@@ -1,34 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ahm.fileupload;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
+import ahn.AHN;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Calendar;
+import javax.ws.rs.*;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.*;
 
-/**
- *
- * @author jacobotapia
- */
-
-@Path("/json/metallica")
+@Path("/file")
 public class RESTController {
     
+    @Context 
+    ServletContext context;
+    
+    private final int limit_max_size = 102400000;
+    private final String limit_type_file = "csv";
+    private final String path_to = "resources/files";
+    
     @POST
-    @Path("/post")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createTrackInJSON() {
-
-        String result = "Track saved : ";
-        return Response.status(201).entity(result).build();
-
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail,
+            @FormDataParam("molecules") int molecules,
+            @FormDataParam("eta") double eta) {
+        
+        String output = "";
+        if(uploadedInputStream!=null && fileDetail!=null){
+            if (this.checkFileType(fileDetail.getFileName())) {
+                String[] data = this.writeToFile(uploadedInputStream, fileDetail.getFileName());
+                AHN.useAHN(data[0], data[0], molecules, eta);
+                output = "Your download code is: " + data[1].split("\\.")[0];
+            } else {
+                output = "Invalid file type";
+            }
+        }else{
+            output = "Please send a csv file";
+        }
+        return Response.status(200).entity(output).build(); 
     }
     
+    private String[] writeToFile(InputStream uploadedInputStream,String submittedFileName) {
+        
+        String currentFileName = submittedFileName;
+        String extension = currentFileName.substring(currentFileName.lastIndexOf("."), currentFileName.length());
+        Long nameRandom = Calendar.getInstance().getTimeInMillis();
+        
+        String newFileName = nameRandom + extension;
+        String fileSavePath = context.getRealPath(path_to)+"/"+newFileName;
+        
+        try {
+            OutputStream out = new FileOutputStream(new File(fileSavePath));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(fileSavePath));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] arr = {fileSavePath,newFileName};
+        return arr;
+    }
+    
+    private boolean checkFileType(String fileName) {
+        if (fileName.length() > 0) {
+            String[] parts = fileName.split("\\.");
+            if (parts.length > 0) {
+                String extension = parts[parts.length - 1];
+                return this.limit_type_file.contains(extension);
+            }
+        }
+        return false;
+    }
+
+
 }
